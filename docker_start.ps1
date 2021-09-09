88f31eadaecf5b6428e5144feaade7073591acc3
Batters <batters1337@gmail.com>
2021-09-09 19:44:43 +0400

    Create Docker Image and Docker container; Create run docker files.

--- docker_start.ps1
+++
@@ -1,2 +0,0 @@
-.\gradlew jibDockerBuild --image=alpha-bank
-docker run -p8080:8080 --name alpha-bank-container -d alpha-bank
--- docker_start.sh
+++ docker_start.sh
@@ -1,2 +1,1 @@
-.\gradlew jibDockerBuild --image=alpha-bank
-docker run -p8080:8080 --name alpha-bank-container -d alpha-bank
+.\gradlew jibDockerBuild --image=alpha-bank && docker run -p8080:8080 --name alpha-bank-container -d alpha-bank
